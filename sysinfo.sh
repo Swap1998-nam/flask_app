ac37b590dff1df6d665170f45a9b29fba8ee00d8
Swapnil Balaganur <swapnilb166@gmail.com>
2026-06-27 13:43:28 +0530

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash 
 echo "Hostname: $(hostname)" 
 echo "Date: $(date)" df -h /
+echo "========== System Information ==========" 
+echo "Hostname: $(hostname)" 
+echo "Date: $(date)" 
+echo 
+
+echo "Disk Usage:" df -h /
